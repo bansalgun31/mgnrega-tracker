021bd79644119af17ac905c84e3a6c0123a4b5e5
Gun Bansal <155285707+bansalgun31@users.noreply.github.com>
2025-10-31 01:48:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -99,6 +99,6 @@ app.get(/.*/, (req, res) => {
 
 // ✅ Start server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () =>
-  console.log(`✅ MGNREGA Server started on port ${PORT}`)
-);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ MGNREGA Server started on port ${PORT}`);
+});
